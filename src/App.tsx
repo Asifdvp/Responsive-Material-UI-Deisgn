b34d96d4009d346a745d878fe74dd6b7d83bef3c
Asif Mustafayev <53805470+Asifdvp@users.noreply.github.com>
2022-06-29 19:27:02 +0400

    Button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-
+import { Button,Stack} from '@mui/material'
+import SettingsIcon from '@mui/icons-material/Settings';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <h1>Material Ui</h1>
+    <Stack spacing={10} direction="row">
+      <Button variant="text" startIcon={<SettingsIcon/>}> Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
     </div>
   )
 }
